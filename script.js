aed10753467d0d9eef403a4e8338d72fb26d6943
MONDSKY <r6544083@gmail.com>
2025-06-05 12:02:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('contact-form').addEventListener('submit', function(e) {
   e.preventDefault();
-  emailjs.sendForm('service_yi1orks', 'template_6neqowc', this)
+  emailjs.sendForm('service_yi1orks', 'template_v0ibur7', this)
     .then(() => alert('Message sent successfully!'))
     .catch((error) => alert('Failed to send message: ' + JSON.stringify(error)));
 });
